badbc54ead55dba156c297537dd4e77434c9d08f
Eddie-Z <35039865+Eddie-Z@users.noreply.github.com>
2019-05-23 22:13:50 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function splitScroll(){
+    const controller = new ScrollMagic.Controller();
+
+    new ScrollMagic.Scene({
+        duration:'100%',
+        triggerElement:'.about-title',
+        triggerHook:0
+    })
+   .setPin('.about-title').addIndicators().addTo(controller);
+}
+splitScroll();
+console.log("hello")
