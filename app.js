469ec51a5fce5b4ac5ea5dfbac9377f0e06cb41d
Eddie Zheng <zhengedd@gmail.com>
2019-05-24 19:18:49 -0700

    bgm, javascript scrolling, paragraph

--- app.js
+++ app.js
@@ -6,7 +6,22 @@ function splitScroll(){
         triggerElement:'.about-title',
         triggerHook:0
     })
-   .setPin('.about-title').addIndicators().addTo(controller);
+   .setPin('.about-title').addTo(controller);
 }
 splitScroll();
+
+window.addEventListener('scroll', ()=>{
+    const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+    const scrolled = window.scrollY;
+    var movieNode = document.getElementById('video');
+    var sound = document.getElementById('sounds');
+   
+
+    if(Math.ceil(scrolled)===scrollable){
+        sound.remove();
+        movieNode.play();
+        
+    }
+
+});
 console.log("hello")
